81a245f03e22ea0f37d58777b286b15d9ccfc351
kristen-kagei <kristen.kagei@gmail.com>
2023-04-25 17:11:23 -0400

    remove function

--- src/Controllers/AbstractOAuth2Controller.php
+++ src/Controllers/AbstractOAuth2Controller.php
@@ -109,23 +109,6 @@ abstract class AbstractOAuth2Controller
         return $this;
     }
 
-     /**
-     * looks for a user's IP address 
-     *
-     * @return $this
-     */
-    public function getClientIP(){       
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)){
-               return  $_SERVER["HTTP_X_FORWARDED_FOR"];  
-        }else if (array_key_exists('REMOTE_ADDR', $_SERVER)) { 
-               return $_SERVER["REMOTE_ADDR"]; 
-        }else if (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
-               return $_SERVER["HTTP_CLIENT_IP"]; 
-        } 
-   
-        return '';
-   }
-
     /**
      * Ensures that externally supplied providers are set
      *
